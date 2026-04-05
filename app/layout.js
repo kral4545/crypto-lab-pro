01ce0f976aaf75517542c8d71424a1acb70f5a70
kral4545 <85643522+kral4545@users.noreply.github.com>
2026-04-05 05:18:22 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Crypto-Lab Trading',
+  description: '0-dan Trading Öğrenme Platformu',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  )
+}
